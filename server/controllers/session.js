b8202629cdafe29ac6196435ccb8ac59a50ea103
zachnieto <nieto.z@husky.neu.edu>
2023-03-30 17:16:13 -0400

    new endpoint names

--- server/controllers/session.js
+++ server/controllers/session.js
@@ -2,19 +2,22 @@ import axios from "axios";
 
 const sessionController = (app) => {
 
-    app.post('/api/session/set/', (req, res) => {
-        const data = req.body.params.data
-        
-        for (const prop in data) {
-            req.session[prop] = data[prop]
-        }
+    app.post('/api/session/set/:name/', (req, res) => {
+        const name = req.params['name'];
+        const body = req.body.params[name]
+
+        req.session[name] = body;
+        res.send(req.session);
 
         console.log(req.session)
-        res.send(req.session);
+    });
+
+    app.get('/api/session/get/:name/', (req, res) => {
+        const value = req.session[req.params['name']];
+        res.send(value);
     });
 
     app.get('/api/session/get/', async (req, res) => {
-
         if (req.session?.auth?.refresh_token) {
             const params = new URLSearchParams({
                 client_id: process.env.CLIENT_ID,
@@ -22,32 +25,25 @@ const sessionController = (app) => {
                 grant_type: 'refresh_token',
                 refresh_token: req.session.auth.refresh_token,
             })
-
             const config = {
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
                 }
             }
-
             await axios.post(`https://discord.com/api/oauth2/token`, params, config).catch(() => {
                 req.session.auth = undefined;
             }).then((resp) => {
                 req.session.auth = resp?.data
             })
         }
-
         res.send(req.session);
     });
-
     app.get('/api/session/reset', (req, res) => {
         req.session.destroy();
         res.send(200);
     });
-
-
     app.post('/api/session/auth/', async (req, res) => {
         const code = req.body.params.code
-
         const params = new URLSearchParams({
             client_id: process.env.CLIENT_ID,
             client_secret: process.env.CLIENT_SECRET,
@@ -55,23 +51,18 @@ const sessionController = (app) => {
             code: code,
             redirect_uri: process.env.DISCORD_REDIRECT_URI
         })
-
         const config = {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         }
-        
+
         await axios.post(`https://discord.com/api/oauth2/token`, params, config).catch(error => {
             req.session.auth = undefined;
-            console.log(error)
         }).then((resp) => {
             req.session.auth = resp?.data
-            console.log(resp?.data)
         })
         res.send(req.session.auth);
     });
-
-
 }
 export default sessionController;
