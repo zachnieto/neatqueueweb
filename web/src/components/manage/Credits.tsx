78241b322eeabb12e42725332839e530a4016239
zachnieto <nieto.z@husky.neu.edu>
2023-10-31 18:22:26 -0400

    hide transfer option

--- web/src/components/manage/Credits.tsx
+++ web/src/components/manage/Credits.tsx
@@ -67,12 +67,12 @@ const Credits = ({
             >
               Buy
             </button>
-              <button
-                  onClick={() => setTransferModalOpen(true)}
-                  className="btn-primary"
-              >
-                  Transfer
-              </button>
+              {/*<button*/}
+              {/*    onClick={() => setTransferModalOpen(true)}*/}
+              {/*    className="btn-primary"*/}
+              {/*>*/}
+              {/*    Transfer*/}
+              {/*</button>*/}
           </div>
       </div>
 
